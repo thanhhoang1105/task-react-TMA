410f76fcdb0ade1014ed44c98f750859277b4b30
thanhhoang1105 <thanhhoangngoc.bmt@gmail.com>
2023-01-05 22:59:29 +0700

    change task 2

--- src/App.js
+++ src/App.js
@@ -1,46 +1,64 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './App.css'
 
 function App() {
+    const [valueInputOne, setValueInputOne] = useState('')
+
     const [valueInputTwo, setValueInputTwo] = useState('')
 
     const [valueKInputTwo, setValueKInputTwo] = useState('')
 
+    const [changeInputOne, setChangeInputOne] = useState('')
     const [changeInputTwo, setChangeInputTwo] = useState('')
 
     const exportInputTaskTwo = (valueInputTwo, valueKInputTwo) => {
         return setChangeInputTwo(
             valueInputTwo && valueInputTwo.length > valueKInputTwo
                 ? valueInputTwo
-                      .slice(0, valueKInputTwo)
+                      .slice(0, valueKInputTwo - 3)
                       .split(' ')
-                      .slice(0, -2)
+                      .slice(0, -1)
                       .join(' ') +
                       ' ' +
                       '...'
-                : valueInputTwo + ' ' + '...'
+                : valueInputTwo
         )
     }
 
-    console.log('valueInputTwo:', valueInputTwo)
-    console.log(
-        'test:',
-        valueInputTwo.slice(0, valueKInputTwo).split(' ').slice(0, -1).join(' ')
-    )
-    // console.log(
-    //     'test',
-    //     valueInputTwo.slice(0, valueKInputTwo).split(' ').slice(0, -2)
-    // )
-    // console.log('changeInputTwo', changeInputTwo)
+    useEffect(() => {
+        if (valueKInputTwo <= 3) {
+            setValueKInputTwo(3)
+        }
+        if (valueKInputTwo >= 500) {
+            setValueKInputTwo(500)
+        }
+    }, [valueKInputTwo])
+
+    const exportInputTaskOne = str => {
+        return setChangeInputOne(
+            str.replace(/[\W_]/g, '').toLowerCase() ===
+                str
+                    .replace(/[\W_]/g, '')
+                    .toLowerCase()
+                    .split('')
+                    .reverse()
+                    .join('')
+        )
+    }
 
     return (
         <div className="Task_container">
             <div className="Task_1">
                 <div>Task 1</div>
-                <input type="text" className="" />
-                <input type="text" className="" />
-                <button>click</button>
-                <div className="Task_result-1">result 1</div>
+                <input
+                    type="text"
+                    className=""
+                    onChange={e => setValueInputOne(e.target.value)}
+                />
+                <button onClick={() => exportInputTaskTwo(valueInputOne)}>
+                    click
+                </button>
+                <div className="Task_result-1">{valueInputOne}</div>
             </div>
             <div className="Task_2">
                 <div>Task 2</div>
@@ -53,8 +71,9 @@ function App() {
                     />
                     <div>K</div>
                     <input
-                        type="text"
+                        type="number"
                         className=""
+                        value={valueKInputTwo}
                         onChange={e => setValueKInputTwo(e.target.value)}
                     />
                 </div>
