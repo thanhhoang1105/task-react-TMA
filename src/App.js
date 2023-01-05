9bc49e3d894cb23e2cbf86d81590e8ee76e1d74b
thanhhoang1105 <thanhhoangngoc.bmt@gmail.com>
2023-01-05 18:52:33 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,74 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react'
+import './App.css'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [valueInputTwo, setValueInputTwo] = useState('')
+
+    const [valueKInputTwo, setValueKInputTwo] = useState('')
+
+    const [changeInputTwo, setChangeInputTwo] = useState('')
+
+    const exportInputTaskTwo = (valueInputTwo, valueKInputTwo) => {
+        return setChangeInputTwo(
+            valueInputTwo && valueInputTwo.length > valueKInputTwo
+                ? valueInputTwo
+                      .slice(0, valueKInputTwo)
+                      .split(' ')
+                      .slice(0, -2)
+                      .join(' ') +
+                      ' ' +
+                      '...'
+                : valueInputTwo + ' ' + '...'
+        )
+    }
+
+    console.log('valueInputTwo:', valueInputTwo)
+    console.log(
+        'test:',
+        valueInputTwo.slice(0, valueKInputTwo).split(' ').slice(0, -1).join(' ')
+    )
+    // console.log(
+    //     'test',
+    //     valueInputTwo.slice(0, valueKInputTwo).split(' ').slice(0, -2)
+    // )
+    // console.log('changeInputTwo', changeInputTwo)
+
+    return (
+        <div className="Task_container">
+            <div className="Task_1">
+                <div>Task 1</div>
+                <input type="text" className="" />
+                <input type="text" className="" />
+                <button>click</button>
+                <div className="Task_result-1">result 1</div>
+            </div>
+            <div className="Task_2">
+                <div>Task 2</div>
+                <div className="Task_Input">
+                    <div>String</div>
+                    <input
+                        type="text"
+                        className=""
+                        onChange={e => setValueInputTwo(e.target.value)}
+                    />
+                    <div>K</div>
+                    <input
+                        type="text"
+                        className=""
+                        onChange={e => setValueKInputTwo(e.target.value)}
+                    />
+                </div>
+                <button
+                    onClick={() =>
+                        exportInputTaskTwo(valueInputTwo, valueKInputTwo)
+                    }
+                >
+                    click
+                </button>
+                <div className="Task_result-2">{changeInputTwo}</div>
+            </div>
+        </div>
+    )
 }
 
-export default App;
+export default App
